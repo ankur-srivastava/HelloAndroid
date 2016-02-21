b1e60cb586f14a779a3df9ee44eb30d4ff6a2940
Ankur Srivastava <ankurlkw@gmail.com>
2016-02-21 08:58:14 +0530

    ListActivity Demo

---
+++ app/src/main/java/com/edocent/helloandroid/ScrollingActivity.java
@@ -0,0 +1,17 @@
+package com.edocent.helloandroid;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ScrollingActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
