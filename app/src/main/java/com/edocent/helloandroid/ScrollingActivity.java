f653a23034a840da0e685b94a72d67fe87362969
Ankur Srivastava <ankurlkw@gmail.com>
2016-02-22 08:32:31 +0530

    ArrayAdapter

--- app/src/main/java/com/edocent/helloandroid/ScrollingActivity.java
+++ app/src/main/java/com/edocent/helloandroid/ScrollingActivity.java
@@ -7,11 +7,20 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ScrollingActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ListView listView = getListView();
+
+        String[] colors = {"Red", "Green", "Pink"};
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, colors);
+        listView.setAdapter(arrayAdapter);
     }
 }
